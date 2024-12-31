4ea15db92b39c1027451aec02f4b944b46580b44
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-31 20:35:11 +0530

    Updated New-File Functions-Params

---
+++ Functions-Params/index2.ts
@@ -0,0 +1,20 @@
+//Default Params :
+//It specifies the value by default to the params of the function
+
+//1.) Regular Function Parameter Annotations
+
+function great(person: string = "Annonymous") {
+  console.log(person);
+}
+
+great();
+great("Nevin");
+
+//2.) ArrowFunctions Parameter Annotations
+
+const greet = (person: string = "Annonymous") => {
+  console.log(person);
+}
+
+greet();
+greet("Nevin");
