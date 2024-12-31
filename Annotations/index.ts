abd1f1630ce565975020fb32ecbc1dc20b885a51
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-31 19:47:38 +0530

    updated Annotations folder

--- Annotations/index.ts
+++ Annotations/index.ts
@@ -1,1 +1,18 @@
-//new updates soon
+//Annotations :
+// Annotations are used to specify the data type of a variaqble, parameter, function return value and other types of values
+
+//String : 
+let myName: string = "Nevin";
+myName = "Another Name";
+
+console.log(myName);
+
+//Number  :
+let myAge: number = 25;
+myAge = 30;
+console.log(myAge);
+
+//Boolean : 
+let isValue: boolean = true;
+isValue = false;
+console.log(isValue);
---
+++ imp.ts
@@ -0,0 +1,3 @@
+// 1.) npm install typescript --save-dev
+// 2.) npx tsc
+// 3.) npm install -g typescript 
