1f892b52e325927b4c3d27b4eb5fc64008a47ce9
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 01:00:50 +0530

    new folder : Objects

---
+++ Objects/index.ts
@@ -0,0 +1,6 @@
+//Object:
+
+//An Object in typeScript is a structured data type that represents a collection of properties, each with a key and an associated value
+//The properties of objects can have specific Type and the object itself can be annotated with a type , often defined as an interface or type alias
+
+
