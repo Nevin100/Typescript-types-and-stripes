f20922d9b5b98255087b9161e471ad81c1c95100
Nevin-Bali100 <opopopopop100000@gmail.com>
2024-12-31 19:54:04 +0530

    updated folder type-inference

---
+++ Type-Inference/index.ts
@@ -0,0 +1,14 @@
+//Type Inference :
+//it is basically a feature in typescript that allows the compiler to automatically determine the type of a variable based on its value
+
+//String : 
+let variable_1 = "Nevin";
+console.log(variable_1); // ts directly inferred the variablke to be a string and no other type can be assigned to this variable
+
+//Number:
+let variabe_2 = 100;
+console.log(variabe_2); // ts directly inferred the variable to be a number and no other type can be assigned to this variable.
+
+//Boolean: 
+let variable_3 = true;
+console.log(variable_3); // ts directly inferred the variable to be a boolean and no other type can be assigned to this variable.
