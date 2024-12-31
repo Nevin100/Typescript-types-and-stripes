893954a846bf8000d32bc19b59e31a12383070cd
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 00:50:06 +0530

    new folder update : ArraysType

---
+++ ArraysType/index.ts
@@ -0,0 +1,16 @@
+//Arrays :
+// Arrays are a type of objectthat can store multiple values of the same data type or in other words of homogenious nature
+
+// Notations : 1.) '[]' and 2.) Array <Type> to indicate an array of a specific type
+
+//1.)Array <Type> notaion:
+
+const names: Array<string> = ["Alice", "Bob"];
+console.log(names);
+
+//2.)Array [] notation
+
+const nums: number[] = [1, 2, 3, 4, 6];
+console.log(nums);
+
+
