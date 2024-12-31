6cc6a91c2835e4d60fc6e720b4f98766845c34ef
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 00:53:40 +0530

    Updated Folder ArraysType

---
+++ ArraysType/index2.ts
@@ -0,0 +1,9 @@
+//Multi-Dinmmensional Arrays  :
+//A Multi Dimmensional array is an array that constains other arrays as its elements.
+
+const matrix: number[][] =[
+  [1, 2],
+  [4, 5],
+]
+
+console.log(matrix);
