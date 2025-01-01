f8bb80c85cf51b755817c6393ddf3a0fc0ebab61
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 11:53:49 +0530

    new folder -> Literals

---
+++ Type-Inference/LiteralsType/index.ts
@@ -0,0 +1,18 @@
+//Literals :
+
+//Literals types allows you to specify a value that can only be onbe specific literal type.
+
+//String : 
+let color1: "red" | "blue" | "green"; 
+color1 = "red";
+console.log(color1); 
+
+//number : 
+let number1: 1 | 2 | 3;
+number1 = 2;
+console.log(number1);
+
+//boolean  :
+let value: true;
+value = true;
+console.log(value);
