a5cc4dc66e600114c4fa63ae7bae78da6d57e6cd
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 12:06:54 +0530

    new update -> Tuples folder

--- Type-Inference/Tuples/index.ts
+++ Type-Inference/Tuples/index.ts
@@ -4,3 +4,10 @@
 let myTuple: [string, number] = ["Hello", 42];
 console.log(myTuple[0]);
 console.log(myTuple[1]);
+
+//Destructuring of a Tuple :
+//It is a way to unpack the elements of a tuple into separate variables.
+
+let [first, second] = myTuple;
+console.log(first);
+console.log(second);
