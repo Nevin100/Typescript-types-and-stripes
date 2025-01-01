31ad6e58dba97c76c7e420cf381ae20a5b161a6f
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 15:32:44 +0530

    OOP Folder Update

---
+++ OOP/index2.ts
@@ -0,0 +1,6 @@
+//Access Specifiers :
+
+//In typescript , you can access modifiers to control the visibility of class members (properties and methods).
+//There are three types of access specifiers in typescript : public, private, protected.
+
+//Public : Members marked as public an be accessed from anywhere, both inside and outside class
