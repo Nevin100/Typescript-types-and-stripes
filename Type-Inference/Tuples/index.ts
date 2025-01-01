ac388b506bb6cb6a65a6b79122b20566c261b6c9
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 12:04:32 +0530

    New Folder Update -> Tuples

---
+++ Type-Inference/Tuples/index.ts
@@ -0,0 +1,6 @@
+//Tuples :
+//It is a type that represents an array with a fixed number of elements, where each element cn have a different type.
+
+let myTuple: [string, number] = ["Hello", 42];
+console.log(myTuple[0]);
+console.log(myTuple[1]);
