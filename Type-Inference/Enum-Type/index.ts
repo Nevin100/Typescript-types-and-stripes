cbeb43c30e4b235c61047858908ef7fdae4d2acd
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 15:18:54 +0530

    new folder-> Enum

--- Type-Inference/Enum-Type/index.ts
+++ Type-Inference/Enum-Type/index.ts
@@ -1,1 +1,11 @@
-//Enums : 
+//Enums :
+
+//Enums is a way to define a set of named constants. Enums allows you to define a collection of related values that can be used interchangeably in your code.
+
+enum WeatherConditions {
+  Sunny = 'sunny',
+  Cloudy = 'cloudy',
+  Rainy = 'rainy',
+}
+
+console.log(WeatherConditions.Cloudy);
