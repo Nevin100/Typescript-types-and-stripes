ac6a01a58a95cb29ae6832725f09dabe8a12918e
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 10:58:21 +0530

    new folder -> Unions

--- Unions/index.ts
+++ Unions/index.ts
@@ -1,6 +1,6 @@
 //Unions
 
-// Unions are used to declare a type that can have one of several possible types Unions are useful when you want to represent a value that can be one of several types.
+// Unions are used to declare a type that can have one of several possible types Unions are useful when you want to represent a value that can be one of several types. (| --> symbol)
 
 let password : string | number = 'hello' 
 password = 45;
