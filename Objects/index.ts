e574d6d32067c80c0f2f5117994c3b723a76c85d
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-01 07:28:56 +0530

    new filue update in  Object

--- Objects/index.ts
+++ Objects/index.ts
@@ -3,4 +3,10 @@
 //An Object in typeScript is a structured data type that represents a collection of properties, each with a key and an associated value
 //The properties of objects can have specific Type and the object itself can be annotated with a type , often defined as an interface or type alias
 
+const person: { firstName: string, lastName: string, age: number } = {
+  firstName: "Nevin",
+  lastName: "Bali",
+  age:20,
+}
 
+console.log(`FirstName : ${person.firstName}, LastName : ${person.lastName}, age:${person.age}`);
