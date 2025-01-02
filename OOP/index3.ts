3da97f277f5594fde104d3176e437ccef7fe100d
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-02 18:21:26 +0530

    Folder Update -> OOp

---
+++ OOP/index3.ts
@@ -0,0 +1,16 @@
+//Getters and Setters :
+
+//Geeters and Setters are used to access and modify the class properties. Getters & Setters are allows you to define a property in class that looks like a simple variable from outside bubt internally has additional logic for getting aqnsd setting of value .
+ 
+class MyClass {
+  private _myProperty: number = 0;
+
+  get myProperty(): number {
+    return this._myProperty
+  }
+
+  set myProperty(value: number) {
+    this._myProperty = value;
+  }
+} const myInstance = new MyClass();
+console.log(`Current Value  :${myInstance.myProperty}`)
