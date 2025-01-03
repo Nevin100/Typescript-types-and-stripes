4f269c24c697dac7c0f31d2eb47c4fc8a292091b
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-01-03 16:47:48 +0530

    new update -> type-narrowing

--- Type-Narrowing/index2.ts
+++ Type-Narrowing/index2.ts
@@ -1,3 +1,5 @@
 // Intersection Types
 
 //It allows you to combine multiple types into single type. the resulting type will have all the properties of each individual type.
+
+//(Code already in type-aliasis of index2.ts)
