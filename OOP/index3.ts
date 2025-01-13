0b7be3728dd325cf31e073e48804bd60962cc127
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-01-13 12:06:37 +0530

    Update index3.ts

--- OOP/index3.ts
+++ OOP/index3.ts
@@ -1,6 +1,6 @@
 //Getters and Setters :
 
-//Geeters and Setters are used to access and modify the class properties. Getters & Setters are allows you to define a property in class that looks like a simple variable from outside bubt internally has additional logic for getting aqnsd setting of value .
+//Geeters and Setters are used to access and modify the class properties. Getters & Setters are allows you to define a property in class that looks like a simple variable from outside but internally has additional logic for getting and setting of value .
  
 class MyClass {
   private _myProperty: number = 0;
